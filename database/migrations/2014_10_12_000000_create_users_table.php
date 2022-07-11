1e5fb9c65d3418c4f78467af2fb012547002adbe
ManmohanAeir <manmohanaeir001110@gmail.com>
2022-07-11 18:30:19 +0545

    adin usertype = 1 default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('usertype')->default(0);
+            $table->string('usertype')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
